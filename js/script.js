89e059452ff0ee7bd900896bdf6b01c072ad7093
lefun4ik <alexey.tsvirkun17@gmail.com>
2022-09-23 18:44:54 +0300

    сверстал слайдер и блок новостей + адаптив

--- js/script.js
+++ js/script.js
@@ -12,3 +12,47 @@ burgerClose.addEventListener('click', () => {
 	menu.classList.remove('header__menu--visible');
 })
 
+$(document).ready(function(){
+	$('.slider').slick({
+		autoplay: true,
+		autoplaySpeed: 1500,
+		arrows: true,
+		dots: true,
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		speed: 350,
+		cssEase: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+		touchThreshold: 15,
+		appendArrows:$('.controls'),
+		appendDots:$('.controls'),
+		responsive: [
+			{
+				breakpoint: 1400,
+				settings: {
+					slidesToShow: 4
+				}
+			},
+			{
+				breakpoint: 1100,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 820,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					autoplay: false,
+					arrows: false,
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+});
