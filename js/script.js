21b4b01064267a0001be408c77f90310852c594b
lefun4ik <alexey.tsvirkun17@gmail.com>
2022-09-25 00:22:07 +0300

    финал

--- js/script.js
+++ js/script.js
@@ -12,6 +12,29 @@ burgerClose.addEventListener('click', () => {
 	menu.classList.remove('header__menu--visible');
 })
 
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+
+			body.classList.remove('_fixed');
+			menu.classList.remove('header__menu--visible');
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
+
 $(document).ready(function(){
 	$('.slider').slick({
 		autoplay: true,
